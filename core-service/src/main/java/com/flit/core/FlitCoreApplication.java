a6d4c59bd4b1c213c8903078e962d723f5053043
Wenceslauu <joaovitor.wenceslau@hotmail.com>
2025-06-22 17:41:19 -0300

    feat(core): set up modular layered architecture with base folders

---
+++ core-service/src/main/java/com/flit/core/FlitCoreApplication.java
@@ -0,0 +1,13 @@
+package com.flit.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlitCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlitCoreApplication.class, args);
+	}
+
+}
---
+++ core-service/src/test/java/com/flit/core/FlitCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flit.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlitCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
