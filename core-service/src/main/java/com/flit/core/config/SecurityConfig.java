d9d6240783696db7f266f6ecae6a0b252cca3eaf
Wenceslauu <joaovitor.wenceslau@hotmail.com>
2025-06-25 15:42:06 -0300

    feat(core): set up spring security for oauth2 resource server

---
+++ core-service/src/main/java/com/flit/core/config/SecurityConfig.java
@@ -0,0 +1,95 @@
+package com.flit.core.config;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {}
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {
+            Map<String, Object> resourceAccess = (Map<String, Object>) claims.get("realm_access");
+            if (resourceAccess == null) return List.of();
+
+            List<String> roles = (List<String>) resourceAccess.get("roles");
+            if (roles == null) return List.of();
+
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+                    .collect(Collectors.toList());
+        };
+    }
+
+    @Bean
+    JwtAuthenticationConverter authenticationConverter(AuthoritiesConverter authoritiesConverter) {
+        var authenticationConverter = new JwtAuthenticationConverter();
+
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            return authoritiesConverter.convert(jwt.getClaims());
+        });
+
+        return authenticationConverter;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http,
+                                                          Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter) throws Exception {
+        http.cors(Customizer.withDefaults());
+
+        http.oauth2ResourceServer(resourceServer -> {
+            resourceServer.jwt(jwtDecoder -> {
+                jwtDecoder.jwtAuthenticationConverter(jwtAuthenticationConverter);
+            });
+        });
+
+        http.sessionManagement(sessions -> {
+            sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }).csrf(csrf -> {
+            csrf.disable();
+        });
+
+        http.authorizeHttpRequests(requests -> {
+            requests.requestMatchers("/public/**").permitAll();
+            requests.anyRequest().authenticated();
+        });
+
+        return http.build();
+    }
+}
